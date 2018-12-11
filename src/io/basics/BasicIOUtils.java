ec630ef937c5bc5d40b78e218aa0c41e3dbdad00
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-12-11 20:08:44 -0200

    implementacao de funcao de descompactacao

---
+++ src/io/basics/BasicIOUtils.java
@@ -0,0 +1,48 @@
+
+package io.basics;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author eduar
+ */
+public abstract class BasicIOUtils {
+    
+    public static final int BUFFER_SIZE = 8192;
+    
+     /**
+     * This method copy the input file to output file
+     * @param from input file
+     * @param to output file
+     * @throws IOException
+     */
+    public static void copy(File from, File to) throws IOException {
+        InputStream in = new FileInputStream(from);
+        OutputStream out = new FileOutputStream(to);
+        copy(in, out);
+        in.close();
+        out.close();
+        to.setLastModified(from.lastModified());
+    }
+
+    /**
+     * This method copy the content of input stream to output stream
+     * @param from input stream
+     * @param to output stream.
+     * @throws IOException
+     */
+    public static void copy(InputStream from, OutputStream to) throws IOException {
+        final int count = BUFFER_SIZE;
+        byte[] bytes = new byte[count];
+        for (int read = -1; (read = from.read(bytes, 0, count)) != -1; to.write(bytes, 0, read))
+            ;
+        to.flush();
+    }
+    
+}
--- src/io/zip/ZipManager.java
+++ src/io/zip/ZipManager.java
@@ -1,12 +1,15 @@
-
 package io.zip;
 
+import io.basics.BasicIOUtils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 import javax.imageio.IIOException;
 
@@ -14,97 +17,211 @@ import javax.imageio.IIOException;
  *
  * @author Eduardo A. Cruz Junior
  */
-public class ZipManager {
-    
-    private static int BUFFER_SIZE = 1024;
-    
+public class ZipManager extends BasicIOUtils {
+
     /**
      * This method compress any file or directory on Zip format
+     *
      * @param inputDirectory directory or file that will be compressed
      * @param outputDirectory directory where the compressed file will be saved
-     * @throws IOException 
+     * @throws IOException
      */
-    public static void zipFile(String inputDirectory, String outputDirectory) throws IOException{
-        
+    public static void zipFile(String inputDirectory, String outputDirectory) throws IOException {
+
         String internalDir = "";
         File file = new File(inputDirectory);
-        if(!file.exists()){
+        if (!file.exists()) {
             throw new IIOException("file not found");
         }
-        
+
         ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(new File(outputDirectory)));
-        
-        if(file.isFile()){
+
+        if (file.isFile()) {
             zip(file, outputDirectory, zipOutputStream);
-        }else{
+        } else {
             internalDir = file.getName();
             File[] files = file.listFiles();
-            for(File f : files){
+            for (File f : files) {
                 zip(f, internalDir, zipOutputStream);
             }
         }
         zipOutputStream.close();
-        
+
     }
-    
+
     /**
      * This method compress any file or directory on Zip format
+     *
      * @param inputDirectory directory or file that will be compressed
      * @param outputDirectory directory where the compressed file will be saved
      * @param attributes list of attributes to add on final archive
-     * @throws IOException 
+     * @throws IOException
      */
-    public static void zipFile(String inputDirectory, String outputDirectory, HashMap<String,String> attributes) throws IOException{
-        
+    public static void zipFile(String inputDirectory, String outputDirectory, HashMap<String, String> attributes) throws IOException {
+
         String internalDir = "";
         File file = new File(inputDirectory);
-        if(!file.exists()){
+        if (!file.exists()) {
             throw new IIOException("file not found");
         }
-        
+
         File outputFile = new File(outputDirectory);
         ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(outputFile));
-        
-        if(file.isFile()){
+
+        if (file.isFile()) {
             zip(file, outputDirectory, zipOutputStream);
-        }else{
+        } else {
             internalDir = file.getName();
             File[] files = file.listFiles();
-            for(File f : files){
+            for (File f : files) {
                 zip(f, internalDir, zipOutputStream);
             }
         }
         zipOutputStream.close();
         AttributeManager.defineAtributes(outputFile, attributes);
-        
+
     }
-    
-    private static void zip(File file, String outputDirectory, ZipOutputStream zipOutputStream) throws IOException{
-        
+
+    private static void zip(File file, String outputDirectory, ZipOutputStream zipOutputStream) throws IOException {
+
         byte data[] = new byte[BUFFER_SIZE];
-        
-        if(file.isDirectory()){
+
+        if (file.isDirectory()) {
             File[] files = file.listFiles();
-            for(File f : files){
+            for (File f : files) {
                 zip(f, outputDirectory + File.separator + file.getName(), zipOutputStream);
             }
-        }else{
-            
+        } else {
+
             FileInputStream fileInputStream = new FileInputStream(file.getAbsolutePath());
             ZipEntry entry = new ZipEntry(outputDirectory + File.separator + file.getName());
             zipOutputStream.putNextEntry(entry);
-            
+
             int count;
-            while((count = fileInputStream.read(data)) > 0){
+            while ((count = fileInputStream.read(data)) > 0) {
                 zipOutputStream.write(data, 0, count);
             }
-            
+
             zipOutputStream.closeEntry();
             fileInputStream.close();
-            
+
         }
-        
+
     }
 
+    /**
+     * This method unzip a file to an directory
+     *
+     * @param inputFile file to unzip
+     * @param outputDirectory outputdirectory
+     * @throws IOException
+     */
+    public static void unzipFile(File inputFile, File outputDirectory) throws IOException {
+
+        if (inputFile.exists()) {
+            if (inputFile.isDirectory()) {
+                throw new IllegalArgumentException("\"" + inputFile.getAbsolutePath() + "\" its is not a file!");
+            }
+        } else {
+            throw new IllegalArgumentException("\"" + inputFile.getAbsolutePath() + "\" does not exist!");
+        }
+        if (outputDirectory.exists()) {
+            if (outputDirectory.isFile()) {
+                throw new IllegalArgumentException("\"" + outputDirectory.getAbsolutePath() + "\" its is not a direcotory!");
+            }
+        }
+
+        ZipInputStream zip = new ZipInputStream(new FileInputStream(inputFile));
+        unzip(zip, outputDirectory);
+        zip.close();
+
+    }
     
+    /**
+     * This method unzip a file to an directory
+     *
+     * @param inputFile file to unzip
+     * @param outputDirectory outputdirectory
+     * @param deleteAfterConclude if true delete the input file after conclude
+     * @throws IOException
+     */
+    public static void unzipFile(File inputFile, File outputDirectory, boolean deleteAfterConclude) throws IOException {
+        unzipFile(inputFile, outputDirectory);
+        if(deleteAfterConclude){
+            inputFile.deleteOnExit();
+        }
+    }
+
+    /**
+     * This method unzip a file to an directory
+     *
+     * @param inputDirectory file to unzip
+     * @param outputDirectory outputdirectory
+     * @throws IOException
+     */
+    public static void unzipFile(String inputDirectory, String outputDirectory) throws IOException {
+        unzipFile(new File(inputDirectory), new File(outputDirectory));
+    }
+
+    /**
+     * This method unzip a file to an directory
+     *
+     * @param inputDirectory file to unzip
+     * @param outputDirectory output directory
+     * @param deleteAfterConclude if true delete the input file after conclude
+     * @throws IOException
+     */
+    public static void unzipFile(String inputDirectory, String outputDirectory, boolean deleteAfterConclude) throws IOException {
+        unzipFile(new File(inputDirectory), new File(outputDirectory), deleteAfterConclude);
+    }
+
+    private static void unzip(ZipInputStream zip, File directory) throws IOException {
+
+        ZipEntry element = null;
+
+        while ((element = zip.getNextEntry()) != null) {
+
+            String name = element.getName();
+            name = name.replace('/', File.separatorChar);
+            name = name.replace('\\', File.separatorChar);
+            File file = new File(directory, name);
+
+            if (element.isDirectory()) {
+                file.mkdirs();
+            } else {
+                if (!file.exists()) {
+                    File parent = file.getParentFile();
+                    if (parent != null) {
+                        parent.mkdirs();
+                    }
+                    file.createNewFile();
+                } else {
+                    if (file.isHidden()) {
+                        Files.setAttribute(file.toPath(), "dos:hidden", false);
+                    }
+                    if (!file.canWrite()) {
+                        file.setWritable(true);
+                    }
+                }
+            }
+
+            OutputStream out = new FileOutputStream(file);
+            copy(zip, out);
+            out.close();
+
+            if (file.exists()) {
+                if (file.isHidden()) {
+                    Files.setAttribute(file.toPath(), "dos:hidden", true);
+                }
+                if (!file.canWrite()) {
+                    file.setWritable(false);
+                }
+            }
+
+            file.setLastModified(element.getTime());
+
+        }
+
+    }
+
 }
