524c1d6d566c0e5547a82fc06c225f19e95b777c
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-12-11 20:13:27 -0200

    implementacao da classe principal para chamada das funcoes do ZipManager

--- src/main/IOUtils.java
+++ src/main/IOUtils.java
@@ -2,15 +2,7 @@
 package main;
 
 import io.zip.ZipManager;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,23 +11,20 @@ public class IOUtils {
     public static void main(String[] args) {
         
         try {
-            HashMap<String, String> atributos = new HashMap();
-            atributos.put("database-version", "4.5");
-            atributos.put("type", "backup-archive");
-            atributos.put("software", "myContab");
-            new ZipManager().zipFile("data", "arquivo.zip", atributos);
-        
+
+            if(args[0].equals("zip")){
+                ZipManager.zipFile(args[1], args[2]);
+            }else if(args[0].equals("unzip")){
+                ZipManager.unzipFile(args[1], args[2]);
+            }else if(args[0].equals("help")){
+                System.out.println("Suported functions: ");
+                System.out.println("-- zip -> to zip an directory");
+                System.out.println("-- unzip -> to unzip a file");
+            }else{
+                throw new IllegalArgumentException("Unsuported function");
+            }
             
-            Path p = new File("arquivo.zip").toPath();
-            
-            final Charset utf8 = StandardCharsets.UTF_8;
-            
-            byte[] attr = (byte[]) Files.readAttributes(p, "user:database-version").get("database-version");
-            String atributo = new String(attr, utf8);
-            System.out.println(atributo);
-            
-            
-        } catch (IOException ex) {
+        }catch(Exception ex) {
             Logger.getLogger(IOUtils.class.getName()).log(Level.SEVERE, null, ex);
         }
         
